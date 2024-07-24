87124ce4977c7e8e2824405a708ece049790b178
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-07-24 14:13:05 -0400

    changed a file

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+import requests
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'my-reddit-app'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {}).get('children', [])
+        for post in data[:10]:
+            print(post['data']['title'])
     else:
-        top_ten(sys.argv[1])
+        print(None)
