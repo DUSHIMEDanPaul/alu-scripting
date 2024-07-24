f22cdae81dbda87731dd62c8a83dc84e8e72f62a
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-07-24 14:34:28 -0400

    new changes

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,6 +1,17 @@
+#!/usr/bin/python3
+"""
+Module to fetch and print the titles of the first 10 hot posts
+from a given subreddit using the Reddit API.
+"""
+
 import requests
 
+
 def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts for a given subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'my-reddit-app'}
     response = requests.get(url, headers=headers, allow_redirects=False)
