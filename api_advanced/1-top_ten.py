f276eae7945118fe8a761625eae15484b63a33da
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-07-24 15:02:29 -0400

    new changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""
-Module to fetch and print the titles of the first 10 hot posts
-from a given subreddit using the Reddit API.
-"""
-
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first
-    10 hot posts for a given subreddit.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'my-reddit-app'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        for post in data[:10]:
-            print(post['data']['title'])
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
