4a752b255602f23c6d88d2a5bd19edc19f01ff68
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-07-24 15:06:20 -0400

    new changes

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """
-Module to fetch and print the titles of the first 10 hot posts
-from a given subreddit using the Reddit API.
+1-main
 """
+import sys
 
-import requests
-
-
-def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first
-    10 hot posts for a given subreddit.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'my-reddit-app'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        for post in data[:10]:
-            print(post['data']['title'])
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print(None)
+        top_ten(sys.argv[1])
